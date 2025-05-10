e4b82cdd9513e6d2fb31f30d0363a66411f3d57d
zhanglu <zhanglu214@huawei.com>
2025-05-10 11:06:03 +0800

    modified the config.json when build container

--- cloud_native/docker_build/docker_build.sh
+++ cloud_native/docker_build/docker_build.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
 FALCONFS_DIR=$DIR/../../
+
+gen_config() {
+    cp -f $FALCONFS_DIR/config/config.json $DIR/Store/
+    JSON_DIR=$DIR/Store/config.json
+    ## modified the content in config.json for container
+    jq '.main.falcon_log_dir = "/opt/log"' $JSON_DIR | sponge $JSON_DIR
+    jq '.main.falcon_cache_root = "/opt/falcon"' $JSON_DIR | sponge $JSON_DIR
+    jq '.main.falcon_mount_path = "/mnt/falcon"' $JSON_DIR | sponge $JSON_DIR
+}
+
 pushd $FALCONFS_DIR
 ./build.sh build pg && ./build.sh install
 ./build.sh build falcon --with-zk-init
@@ -13,9 +22,17 @@ mkdir -p $DIR/Store/falconfs/lib/
 ./ldd_copy.sh -b ~/metadb/lib/postgresql/falcon.so -t ~/metadb/lib/
 ./ldd_copy.sh -b $FALCONFS_DIR/build/bin/falcon_client -t $DIR/Store/falconfs/lib/
 
-cp -f $FALCONFS_DIR/build/bin/falcon_client $DIR/Store/falconfs/bin/
 cp -rf ~/metadb ./CN/
 cp -rf ~/metadb ./DN/
 cp -rf $FALCONFS_DIR/cloud_native/falcon_cm ./CN/
 cp -rf $FALCONFS_DIR/cloud_native/falcon_cm ./DN/
+
+cp -f $FALCONFS_DIR/build/bin/falcon_client $DIR/Store/falconfs/bin/
+
+chmod 777 -R ./CN/metadb
+chmod 777 -R ./CN/falcon_cm
+chmod 777 -R ./DN/metadb
+chmod 777 -R ./DN/falcon_cm
+chmod 777 -R ./Store/falconfs
+gen_config
 popd
