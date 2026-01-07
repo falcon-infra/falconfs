e8cc524e05f075ce402c2e1bb175491b2b7ff0ca
sth4nthL <liumingyu15@huawei.com>
2026-01-07 16:49:43 +0800

    refine falcon_env.sh

--- deploy/falcon_env.sh
+++ deploy/falcon_env.sh
@@ -6,5 +6,6 @@ export FALCONFS_HOME="${SCRIPT_DIR}/.."
 echo "Setting FALCONFS_HOME to ${FALCONFS_HOME}"
 export CONFIG_FILE=$FALCONFS_HOME/config/config.json
 export FALCONFS_INSTALL_DIR="${FALCONFS_INSTALL_DIR:-/usr/local/falconfs}"
-export PG_INSTALL_DIR="$FALCONFS_INSTALL_DIR/falcon_metadb"
 export PATH=$FALCONFS_INSTALL_DIR/bin:$PATH
+
+export PG_INSTALL_DIR="$(pg_config --libdir)/.."
